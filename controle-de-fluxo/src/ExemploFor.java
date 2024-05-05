7df44cc95586fae3c4c8006b59b755073b276644
ValmirNogueira <valmirnogueiraifrn@hotmail.com>
2024-05-05 10:53:08 -0300

    exercicios

---
+++ controle-de-fluxo/src/ExemploFor.java
@@ -0,0 +1,8 @@
+public class ExemploFor {
+    public static void main(String[] args) {
+       
+        for(int par = 0; par <= 101;par = par + 2){
+            System.out.println(par);
+        }
+    }
+}
--- controle-de-fluxo/src/ResultadoEscolar.java
+++ controle-de-fluxo/src/ResultadoEscolar.java
@@ -1,12 +1,10 @@
 public class ResultadoEscolar {
     public static void main(String[] args) {
-        int nota = 7;
+        int nota = 3;
        
-        if(nota >= 7)
-         System.out.println("Aprovado");
- 
-        else
-         System.out.println("Reprovado");
+       String resultado = nota >=7 ? "Aprovado" : nota >=5 && nota <7 ? " Possível recuperação ": nota >=4 && nota < 5? "Recuperação direto": " Reprovado";
+   System.out.println(resultado);
+   
     }
     
 }
